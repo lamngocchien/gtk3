060911e21f283370ad17fb4dcb7a7fced587246d
lamngocchien <lamngocchien@gmail.com>
2018-12-30 14:21:07 +0900

    Remove unnessary code

--- first/first.py
+++ first/first.py
@@ -39,62 +39,9 @@ class MenuExampleWindow(Gtk.Window):
 
     def __init__(self):
         Gtk.Window.__init__(self, title=ProgramName)
-        # self.set_default_size(ProgramWidth, ProgramHeight)
-        action_group = Gtk.ActionGroup("my_actions")
+        self.set_default_size(ProgramWidth, ProgramHeight)
         self.user_edited_new_document = True
-        # File Menu
-        action_filemenu = Gtk.Action("FileMenu", "Setting", None, None)
-        action_group.add_action(action_filemenu)
-
-        # action_filenew = Gtk.Action("FileNew", "New", None, None)
-        # action_filenew.connect("activate", self.on_menu_file_new)
-        # action_group.add_action(action_filenew)
-        #
-        # action_open = Gtk.Action("FileOpen", "Open", None, None)
-        # action_open.connect("activate", self.on_menu_file_open)
-        # action_group.add_action(action_open)
-        #
-        # action_filequit = Gtk.Action("FileQuit", None, None, Gtk.STOCK_QUIT)
-        # action_filequit.connect("activate", self.on_menu_file_quit)
-        # action_group.add_action(action_filequit)
-
-        # Edit Menu
-        # action_filemenu = Gtk.Action("EditMenu", "Edit", None, None)
-        # action_group.add_action(action_filemenu)
-        #
-        # action_filenew = Gtk.Action("EditCopy", "Copy", None, None)
-        # action_filenew.connect("activate", self.on_menu_file_new)
-        # action_group.add_action(action_filenew)
-        #
-        # action_open = Gtk.Action("EditPaste", "Paste", None, None)
-        # action_open.connect("activate", self.on_menu_file_open)
-        # action_group.add_action(action_open)
-        #
-        # action_filequit = Gtk.Action("EditSomething", "Something", None, None)
-        # action_filequit.connect("activate", self.on_menu_file_quit)
-        # action_group.add_action(action_filequit)
-
-        # About
-        action_about = Gtk.Action("About", " About ", 5, None)
-        action_about.connect("activate", self.on_menu_about)
-        action_about.set_tooltip('About')
-        action_group.add_action(action_about)
-        # End
-
-        # uimanager = self.create_ui_manager()
-        uimanager = Gtk.UIManager()
-
-        # Throws exception if something went wrong
-        uimanager.add_ui_from_string(UI_INFO)
-
-        # Add the accelerator group to the toplevel window
-        accelgroup = uimanager.get_accel_group()
-        self.add_accel_group(accelgroup)
-
-        uimanager.insert_action_group(action_group)
-        menubar = uimanager.get_widget("/MenuBar")
-
-
+        self.file_name_default = 'result.xlsx'
         #Main SiderBar
         grid = Gtk.Grid()
         self.add(grid)
@@ -110,42 +57,28 @@ class MenuExampleWindow(Gtk.Window):
         grid.attach(stacksidebar, 0, 0, 1, 1)
 
 
-
-
+        # Measurement Compare Tab
         box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=8)
-        box_menu = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=8)
+        # box_menu = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=8)
         box_content = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=8)
 
-        box_content_blank =  Gtk.Box(spacing=10)
+        box_content_blank = Gtk.Box(spacing=10)
         box_content_blank.set_size_request(1,15)
         box_content_main = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=8)
         box_content.pack_start(box_content_blank, False, False, 0)
         box_content.pack_start(box_content_main, False, False, 0)
 
         # box.pack_start(box_menu, False, False, 0)
-        box_menu.pack_start(menubar, False, False, 0)
+        # box_menu.pack_start(menubar, False, False, 0)
         box.pack_start(box_content, False, False, 0)
 
-        # label = Gtk.Label("Stack Content on Page %i" % (page))
-
-
-        # Add button
-        # self.add(box)
         stack.add_titled(box, 'Measurement Compare', 'Measurement Compare')
-
-        # layout2 = Gtk.Box(spacing=10)
-        # box.add(layout)
 
         # button 1
         button1 = Gtk.Button("Select Old")
-        button1.connect("clicked", self.button_clicked1)
-        # button1.set_size_request(120,30)
-
-        # button 2
+        button1.connect("clicked", self.measurement_compare_upload1)
         button2 = Gtk.Button("Select New")
-        button2.connect("clicked", self.button_clicked2)
-        # button2.set_size_request(120, 30)
-
+        button2.connect("clicked", self.measurement_compare_upload2)
 
         # Path 1
         self.path1 = Gtk.Entry()
@@ -156,55 +89,32 @@ class MenuExampleWindow(Gtk.Window):
         self.path2.set_property("width-request", 450)
         self.path2.set_text("")
 
-
         # Add some config
         layout3 = Gtk.Box(spacing=10)
         layout3.set_homogeneous(False)
         vbox_left = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=10)
-        # vbox_left.set_properties("width-request", 120)
         vbox_left.set_homogeneous(False)
         vbox_right = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=10)
-        # vbox_right.set_margin_top(10)
-        # vbox_right.set_margin_left(10)
         vbox_right.set_margin_right(10)
         vbox_right.set_homogeneous(False)
         # Make two columns
         layout3.pack_start(vbox_left, True, True, 0)
         layout3.pack_start(vbox_right, True, True, 0)
 
-
-
         # Add to layout
         label1 = Gtk.Label("Old Prompt")
-        # label1.set_property("width-request", 120)
-        label1.set_justify(Gtk.Justification.RIGHT)
-
         label2 = Gtk.Label("New Prompt")
-        # label2.set_property("width-request", 120)
-        label2.set_justify(Gtk.Justification.RIGHT)
-
-
 
         self.first_prompt = Gtk.Entry()
         self.first_prompt.set_property("width-request", 450)
         self.first_prompt.set_text("")
-
         # Path 2
         self.second_prompt = Gtk.Entry()
         self.second_prompt.set_property("width-request", 450)
         self.second_prompt.set_text("")
 
-
-
         label3 = Gtk.Label("Old Version")
-        # label3.set_property("width-request", 120)
-        label3.set_justify(Gtk.Justification.RIGHT)
-
         label4 = Gtk.Label("New Version")
-        # label4.set_property("width-request", 120)
-        label4.set_justify(Gtk.Justification.RIGHT)
-
-
 
         self.first_version = Gtk.Entry()
         self.first_version.set_property("width-request", 450)
@@ -215,8 +125,6 @@ class MenuExampleWindow(Gtk.Window):
         self.second_version.set_property("width-request", 450)
         self.second_version.set_text("")
 
-        # vbox_left.pack_start(Gtk.Label(""), True, True, 0)
-        # vbox_right.pack_start(Gtk.Label(""), True, True, 7)
         button1.set_margin_top(10)
         self.path1.set_margin_top(10)
         vbox_left.add(button1)
@@ -226,10 +134,6 @@ class MenuExampleWindow(Gtk.Window):
         vbox_left.pack_start(label3, True, True, 0)
         vbox_right.pack_start(self.first_version, True, True, 0)
 
-
-
-        # vbox_left.pack_start(Gtk.Label(""), True, True, 0)
-        # vbox_right.pack_start(Gtk.Label(""), True, True, 8)
         button2.set_margin_top(10)
         vbox_left.add(button2)
         self.path2.set_margin_top(10)
@@ -240,15 +144,9 @@ class MenuExampleWindow(Gtk.Window):
         vbox_left.pack_start(label4, True, True, 0)
         vbox_right.pack_start(self.second_version, True, True, 0)
 
-        # vbox_left.pack_start(Gtk.Label(""), True, True, 0)
-        # vbox_right.pack_start(Gtk.Label(""), True, True, 8)
-
         label5 = Gtk.Label("Select Config File")
-        # label5.set_margin_top(15)
-        # label5.set_property("width-request", 120)
         label5.set_justify(Gtk.Justification.RIGHT)
         vbox_left.pack_start(label5, True, True, 0)
-
 
 
         self.config = 'config.json'
@@ -263,58 +161,36 @@ class MenuExampleWindow(Gtk.Window):
 
         button_update = Gtk.Button("Reload Config")
         button_update.connect("clicked", self.update_config_file)
-        # vbox_right.pack_start(button_update, False, False, True)
-
-        # box_content_main.pack_start(layout1, False, False, 0)
-        # box_content_main.pack_start(layout2, False, False, 0)
-        # Progress Bar
-        # layout1 = Gtk.Box(spacing=10)
-        # self.updating = True
-        # self.progressbar = Gtk.ProgressBar()
-        # layout1.pack_start(self.progressbar, True, True, 0)
-
-        # self.levelbar = Gtk.LevelBar()
-        # self.levelbar.set_min_value(0)
-        # self.levelbar.set_max_value(100)
-        # self.levelbar.set_value(0)
-
-        # layout1.pack_start(self.levelbar, True, True, 0)
-        # box_content_main.pack_start(layout1,  False, False, 0)
-        # box_content_main.pack_start(layout1,  False, False, 0)
         box_content_main.pack_start(layout3,  False, False, 0)
-        # vbox_left.pack_start(Gtk.Label(""), True, True, 0)
-        # vbox_right.pack_start(Gtk.Label(""), True, True, 8)
-
 
         layout_last = Gtk.Box(spacing=10)
         layout_last.set_margin_right(10)
         layout_last.set_margin_bottom(5)
         button_last = Gtk.Button("Comparasion")
         button_last.connect("clicked", self.button_compare)
-        # button_last.set_size_request(120,30)
         label_last = Gtk.Label("")
         label_last.set_property("width-request", 400)
-        # label_last.set_property("height-request", 40)
         layout_last.add(label_last)
         layout_last.add(button_update)
         layout_last.add(button_last)
         box_content_main.pack_start(layout_last, False, False, 10)
+        # End Measurement Compare Tab
 
-        self.file_name_default = 'result.xlsx'
-
+        # Configuration Compare Tab
         label = Gtk.Label("Under Contruction")
         stack.add_titled(label, 'Configuration Compare', 'Configuration Compare')
+        # End Configuration Compare Tab
+
+
+        # Excel to Json
         label = Gtk.Label("Under Contruction")
-        stack.add_titled(label, 'Excel To Json', 'Excel To Json')
-        # label = Gtk.Label("Under Contruction")
+        stack.add_titled(label, 'Excel to Json', 'Excel to Json')
+        # End Excel to Json
+
+        # About Tab
         about_box = Gtk.Box(spacing=10, orientation=Gtk.Orientation.VERTICAL)
         about_box.set_homogeneous(False)
         about_box.set_border_width(10)
-        # about_box.set_size_request(500,400)
-        # about_box.set_margin_top(10)
-        # about_box.set_margin_left(10)
-        # about_box.set_margin_right(10)
-        # about_box.set_margin_bottom(10)
         label = Gtk.Label()
         label.set_text("CorePyTool"
                        "\n_________________________________________________________________"
@@ -324,10 +200,9 @@ class MenuExampleWindow(Gtk.Window):
                        "\nPublished on 2 Jan 2019"
                        "\nSupport: Some kind of measurement on (v)MME, (v)GW")
         label.set_justify(Gtk.Justification.LEFT)
-
         about_box.pack_start(label, False, False, 0)
-
         stack.add_titled(about_box, 'About', 'About')
+        # End About Tab
 
     def update_config_file(self, widget):
         print 'Clicked Update Config'
@@ -476,27 +351,7 @@ class MenuExampleWindow(Gtk.Window):
 
         workbook.close()
 
-    def on_menu_file_quit(self, widget):
-        Gtk.main_quit()
-
-    def on_menu_file_open(self, widget):
-        print("Open on File Menus")
-
-    def on_menu_about(self, widget):
-        print("About on File Menus")
-        dialog = PopUp(self)
-        dialog.set_default_size(400, 75)
-        respone = dialog.run()
-        if respone == Gtk.ResponseType.OK:
-            print "OK"
-        elif respone == Gtk.ResponseType.CANCEL:
-            print "Cancel"
-        dialog.destroy()
-
-    def on_menu_file_new(self, widget):
-        print("New on File Menus")
-
-    def button_clicked1(self, widget):
+    def measurement_compare_upload1(self, widget):
         print "Go to button_clicked1"
         dialog = Gtk.FileChooserDialog("Select a file", self, Gtk.FileChooserAction.OPEN,
                                        ("Cancel", Gtk.ResponseType.CANCEL,
@@ -517,7 +372,7 @@ class MenuExampleWindow(Gtk.Window):
             print ("Cancel Clicked")
         dialog.destroy()
 
-    def button_clicked2(self, widget):
+    def measurement_compare_upload2(self, widget):
         print "Go to button_clicked2"
         dialog = Gtk.FileChooserDialog("Select a file", self, Gtk.FileChooserAction.OPEN,
                                        ("Cancel", Gtk.ResponseType.CANCEL,
@@ -535,20 +390,6 @@ class MenuExampleWindow(Gtk.Window):
         elif response == Gtk.ResponseType.CANCEL:
             print ("Cancel Clicked")
         dialog.destroy()
-
-
-class PopUp(Gtk.Dialog):
-    def __init__(self, parent):
-        Gtk.Dialog.__init__(self, "About", parent, Gtk.DialogFlags.MODAL, (
-            # "Custom cancel text", Gtk.ResponseType.CANCEL,
-            # Gtk.STOCK_OK, Gtk.ResponseType.OK,
-        ))
-        self.set_border_width(10)
-        self.set_size_request(300, 100)
-        area = self.get_content_area()
-        area.add(Gtk.Label("Measurement Compare Tool for Performance Team\nVersion 1.0.0\nDeveloped by lam.chien\nPublished on 2 Jan 2019"
-                           "\nSupport: Some kind of measurement on (v)MME, (v)GW"))
-        self.show_all()
 
 
 window = MenuExampleWindow()
